8affdedbc93e07dfbae69bf297056937155752f1
Gregory Gray <gregory.gray@summitcommunication.com.au>
2026-03-16 10:28:13 +1000

    Create Summit theme starter structure

--- theme/summit/functions.php
+++ theme/summit/functions.php
@@ -1,2 +1,29 @@
 <?php
-// Summit theme functions
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+function summit_theme_setup() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('editor-styles');
+    add_theme_support('wp-block-styles');
+    add_theme_support('responsive-embeds');
+
+    register_nav_menus([
+        'primary' => __('Primary Menu', 'summit'),
+        'footer'  => __('Footer Menu', 'summit'),
+    ]);
+}
+add_action('after_setup_theme', 'summit_theme_setup');
+
+function summit_enqueue_assets() {
+    wp_enqueue_style(
+        'summit-style',
+        get_stylesheet_uri(),
+        [],
+        wp_get_theme()->get('Version')
+    );
+}
+add_action('wp_enqueue_scripts', 'summit_enqueue_assets');
--- theme/summit/index.php
+++ theme/summit/index.php
@@ -1,3 +1,8 @@
-<?php
-echo "<h1>Summit Theme is running</h1>";
-?>
+<?php get_header(); ?>
+
+<main style="padding: 40px; font-family: Arial, sans-serif;">
+    <h1>Summit Theme is running</h1>
+    <p>The custom WordPress theme is active.</p>
+</main>
+
+<?php get_footer(); ?>
